0c62f46a4e408be319cad81559e9d9b8a9824212
iiamuser <yeach01@naver.com>
2022-07-25 00:06:41 +0900

    JJIn last

--- components/Title.tsx
+++ components/Title.tsx
@@ -3,7 +3,7 @@ import { FC, useState } from "react";
 import { Link as ScrollLink } from "react-scroll";
 
 // @ 타이틀 이미지는 이미지에 마우스를 올리면 Gif 이미지가 나오도록 하였습니다. public/images 교체하셔서 사용하시면 됩니다.
-const titleImage = "logo1.png";
+const titleImage = "logo3.png";
 const titleImageGif = "logo2.png";
 
 const Title: FC = () => {
